739f1e612a66655e96f1230b97864d1c5b9242a2
jafar-sweity <jata1518@gmail.com>
2024-10-12 16:07:05 +0300

    bug fixed in CI

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,16 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // For browser globals
+        ...globals.node, // For Node.js globals (e.g., process, require)
+        ...globals.jest, // Add this if you're using Jest for testing
+        // If you're using Mocha, replace `globals.jest` with `globals.mocha`
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
